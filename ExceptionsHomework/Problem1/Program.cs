a73a2fa15a991735e82e49334864a881aee09219
Daniel <dan13lstz@abv.bg>
2020-04-04 00:17:36 +0300

    Problem1 added

---
+++ ExceptionsHomework/Problem1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static double SqrtNumber(int number)
+        {
+            double result = 0;
+            if (number >= 0)
+            {
+                result = Math.Sqrt((double)number);
+            }
+            else
+            {
+                result = Math.Sqrt((double)number);
+                throw new System.ArgumentOutOfRangeException("Negative number!");
+
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Напишете цяло число:");
+                int number = int.Parse(Console.ReadLine());
+                double result = SqrtNumber(number);
+                Console.WriteLine("Sqrt({0}) = {1}", number, result);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.ParamName);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Invalid number(OverflowException)");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Invalid number(FormatException)");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
